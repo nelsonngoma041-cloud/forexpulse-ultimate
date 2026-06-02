25619aa10de030e6f3ee11f9cb2f7e0ee421584b
nelsonngoma041-cloud <nelsonngoma041@gmail.com>
2026-06-02 12:32:56 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
-  images: {
-    domains: [],
-  },
+  // Remove experimental.serverActions - it's now default
 }
 
 module.exports = nextConfig
